4437650167416402708edd70f23ce8ff5d27d3bc
waitme321 <zhangyongweb@163.com>
2018-11-14 16:32:52 +0800

    11
    Signed-off-by: waitme321 <zhangyongweb@163.com>

--- js/modes/ordertable.js
+++ js/modes/ordertable.js
@@ -1079,7 +1079,7 @@ layui.define(['layer', 'form', "table", "laypage", "element", "laytpl", "laydate
 
 
 
-    exports('ordertable', {}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致123123231231231321
-    
+    exports('ordertable', {}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致1231232312
+
 });
 //提交
